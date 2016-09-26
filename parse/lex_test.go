f90dda80756f96218f49d59b6b8ab913e22fbb6e
Jemma Nelson <jemma.s.nelson@gmail.com>
2016-09-25 22:14:30 -0700

    Marginally improve lex testing setup

--- parse/lex_test.go
+++ parse/lex_test.go
@@ -3,21 +3,31 @@ package parse
 import "fmt"
 import "testing"
 
-func logLex(s string) {
-	fmt.Printf("Lexing:\n%s\n\n", s)
+var shouldLex = [...]string{
+	`{print}`,
+	` /Err\/die/ `,
+	` { x += $2; $3 = $1>>2 ; print } `,
+	` { " Hi I'm \" georigiono" } `,
+	`NF%2 == 1`,
+}
+
+func lexes(s string, t *testing.T) {
 	l := lex(s)
 	n := l.nextItem()
 	for ; n.yys != int(itemEOF); n = l.nextItem() {
-		fmt.Printf("%-20s %#v\n", n, n)
+		t.Logf("%-20s %#v\n", n, n)
+		if n.yys == int(itemError) {
+			t.Error(n)
+		}
 	}
-	fmt.Println(n)
+	t.Log(n)
 }
 
 func TestIt(t *testing.T) {
-	logLex(`{print}`)
-	logLex(` /Err\/die/ `)
-	logLex(` { x += $2; $3 = $1>>2 ; print } `)
-	logLex(` { " Hi I'm \" georigiono" } `)
-	logLex(`NF%2 == 1`)
+	for _, s := range shouldLex {
+		t.Run("ShouldLex"+s, func(t *testing.T) {
+			lexes(s, t)
+		})
+	}
 	fmt.Println("done")
 }
